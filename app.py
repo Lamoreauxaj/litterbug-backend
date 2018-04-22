afb69d471b471e1490f80bc1302c5ba8d75e4a81
Aaron Lamoreaux <LamoreauxAJ@gmail.com>
2018-04-22 08:11:16 -0500

    updated session

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask.ext.session import Session
+# from flask.ext.session import Session
 from config.main import SECRET_KEY
 
 # Flask Setup
 app = Flask(__name__)
-app.config['SERVER_NAME'] = 'dev.localhost'
-app.config['SESSION_TYPE'] = 'filesystem'
+# app.config['SERVER_NAME'] = 'dev.localhost'
+# app.config['SESSION_TYPE'] = 'filesystem'
 app.secret_key = SECRET_KEY
-app.config['SESSION_COOKIE_DOMAIN'] = 'http://dev.localhost:8080'
+# app.config['SESSION_COOKIE_DOMAIN'] = 'http://dev.localhost:8080'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///litterbug.db'
 app.config['SECRET_KEY'] = SECRET_KEY
 db = SQLAlchemy(app)
-Session(app)
+# Session(app)
 
 # Import models
 import models.user
